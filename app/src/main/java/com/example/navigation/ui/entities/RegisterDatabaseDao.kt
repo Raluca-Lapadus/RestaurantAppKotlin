6d1a2184bc2ae8081b73b51ca6bcea63a5b0772c
Lapadus <raluca.lapadus13@gmail.com>
2024-07-07 16:31:19 +0300

    login

---
+++ app/src/main/java/com/example/navigation/ui/entities/RegisterDatabaseDao.kt
@@ -0,0 +1,22 @@
+package com.example.navigation.ui.entities
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+interface RegisterDatabaseDao {
+
+    @Insert
+    suspend fun insert(register: RegisterEntity)
+
+    @Query("SELECT * FROM Register_users_table ORDER BY userId DESC")
+    fun getAllUsers(): LiveData<List<RegisterEntity>>
+
+    @Query("SELECT * FROM Register_users_table WHERE user_name LIKE :userName")
+    suspend fun getUsername(userName: String): RegisterEntity?
+
+}
---
+++ app/src/main/java/com/example/navigation/ui/repositories/RegisterRepository.kt
@@ -0,0 +1,19 @@
+package com.example.navigation.ui.repositories
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.example.navigation.ui.entities.RegisterDatabaseDao
+import com.example.navigation.ui.entities.RegisterEntity
+
+class RegisterRepository(private val dao: RegisterDatabaseDao) {
+
+    val users = dao.getAllUsers()
+
+    suspend fun insert(user: RegisterEntity) {
+        return dao.insert(user)
+    }
+
+    suspend fun getUserName(userName: String): RegisterEntity?{
+        return dao.getUsername(userName)
+    }
+}
